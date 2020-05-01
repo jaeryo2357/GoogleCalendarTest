332f65770eb3b6d96ac882137c0def7ed1c8a3db
jaeryo2357 <jaeryo2357@naver.com>
2020-05-01 15:12:58 +0900

    init project

---
+++ app/src/main/java/com/mut_jaeryo/googlecalendartest/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.mut_jaeryo.googlecalendartest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
